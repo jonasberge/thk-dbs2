8df862e67429d708d24c794c2906efecefcd208c
Edis Vilja <edis.vilja@smail.th-koeln.de>
2020-05-21 22:51:05 +0200

    MySQL test script erstellt + GruppeBeitritt Trigger

---
+++ test_mysql.sql
@@ -0,0 +1,70 @@
+-- region DELETE
+
+DELETE FROM Gruppe_Student;
+DELETE FROM GruppenBeitrag;
+DELETE FROM Gruppe;
+DELETE FROM Student;
+DELETE FROM Modul;
+DELETE FROM Studiengang;
+DELETE FROM Fakultaet;
+
+-- endregion
+
+-- region [Test] Trigger GruppeBeitritt
+
+-- region [setup]
+
+INSERT INTO Fakultaet (ID, NAME, STANDORT)
+VALUES (1, 'Informatik', 'Gummersbach');
+
+INSERT INTO Studiengang (ID, NAME, FAKULTAET_ID, ABSCHLUSS)
+VALUES (1, 'Informatik', 1, 'BSC.INF');
+
+INSERT INTO Modul (ID, NAME, DOZENT, SEMESTER)
+VALUES (1, 'Mathematik 1', 'Wolfgang Konen', 1);
+
+INSERT INTO Student (ID, NAME, SMAIL_ADRESSE, PASSWORT_HASH,
+                     PROFIL_BESCHREIBUNG, PROFIL_BILD, STUDIENGANG_ID, SEMESTER)
+SELECT 1, 'Frank', 'frank@th-koeln.de', 'h', 'Ich mag Informatik.',              NULL, 1, 1 FROM dual UNION
+SELECT 2, 'Peter', 'peter@th-koeln.de', 'h', 'Ich bin Technologie-begeistert.',  NULL, 1, 1 FROM dual UNION
+SELECT 3, 'Hans',   'hans@th-koeln.de', 'h', 'Tortillas sind meine Spezialität', NULL, 1, 1 FROM dual;
+
+INSERT INTO Gruppe (ID, MODUL_ID, ERSTELLER_ID, NAME)
+SELECT 1, 1, 1 /* Frank */, 'Mathe-Boyz'    FROM dual UNION
+SELECT 2, 1, 1 /* Frank */, 'Mathe-Boyz #2' FROM dual UNION
+SELECT 3, 1, 2 /* Peter */, 'Math Rivals'   FROM dual;
+
+UPDATE Gruppe SET betretbar = '1' WHERE id = 1;
+
+-- endregion
+
+-- Beitritt nur bei bestätigter Anfrage möglich
+INSERT INTO Gruppe_Student (GRUPPE_ID, STUDENT_ID, BEITRITTSDATUM)
+SELECT 2, 1, NOW() FROM dual;
+
+INSERT INTO GruppenAnfrage (GRUPPE_ID, STUDENT_ID, BESTAETIGT, DATUM)
+SELECT 2, 1, '1', NOW() FROM dual;
+
+INSERT INTO Gruppe_Student (GRUPPE_ID, STUDENT_ID, BEITRITTSDATUM)
+SELECT 2, 1, NOW() FROM dual;
+
+-- Beitritt nur vor Deadline möglich
+UPDATE Gruppe SET deadline = NOW() - INTERVAL 1 DAY WHERE id = 1;
+INSERT INTO Gruppe_Student (GRUPPE_ID, STUDENT_ID, BEITRITTSDATUM)
+SELECT 1, 1, NOW() FROM dual;
+
+-- Insert nur möglich wenn Limit an Mitgliedern nicht überschritten wird
+UPDATE Gruppe SET betretbar = '1' WHERE id = 3;
+UPDATE Gruppe SET `limit` = 2 WHERE id = 3;
+INSERT INTO Gruppe_Student (GRUPPE_ID, STUDENT_ID, BEITRITTSDATUM)
+SELECT 3, 1, NOW() FROM dual UNION
+SELECT 3, 2, NOW() FROM dual UNION
+SELECT 3, 3, NOW() FROM dual;
+
+-- region [teardown]
+
+ROLLBACK;
+
+-- endregion
+
+-- endregion
